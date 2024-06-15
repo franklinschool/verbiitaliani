f6797840d4166e30e9a35dbb26fa446a0c6c41ff
franklinschool <bryan.mulligan@gmail.com>
2024-06-15 17:52:17 -0500

    added new files

--- sw.js
+++ sw.js
@@ -1,23 +1,19 @@
-const cacheName = 'VerbiItaliani';
+const cacheName = 'verbiItaliani';
 
-// Cache all the files to make a PWA
 self.addEventListener('install', e => {
   e.waitUntil(
     caches.open(cacheName).then(cache => {
-      // Our application only has two files here index.html and manifest.json
-      // but you can add more such as style.css as your app grows
       return cache.addAll([
         './',
         './index.html',
-        './manifest.json'
+        './manifest.json',
+        './style.css',
+        './script.js',
       ]);
     })
   );
 });
 
-// Our service worker will intercept all fetch requests
-// and check if we have cached the file
-// if so it will serve the cached file
 self.addEventListener('fetch', event => {
   event.respondWith(
     caches.open(cacheName)
