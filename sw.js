eef232c82a3a944546ce21c6ef9ee25588bf726c
franklinschool <bryan.mulligan@gmail.com>
2024-06-15 19:49:31 -0500

    cache path fix

--- sw.js
+++ sw.js
@@ -1,23 +1,24 @@
 const cacheName = 'verbiItaliani';
 const assets = [
   '/',
-  '/index.html',
-  '/public/style.css',
-  '/public/script.js',
-  '/public/materialize.min.css',
-  '/public/materialize.min.js',
-  '/public/manifest.json',
+  './index.html',
+  './public/style.css',
+  './public/script.js',
+  './public/materialize.min.css',
+  './public/materialize.min.js',
+  './public/manifest.json',
   'https://fonts.googleapis.com/icon?family=Material+Icons',
   'https://fonts.gstatic.com/s/materialicons/v142/flUhRq6tzZclQEJ-Vdg-IuiaDsNcIhQ8tQ.woff2',
 ];
 
-self.addEventListener('install', evt => {
-  evt.waitUntil(
-    caches.open(cacheName).then(cache => {
-      cache.addAll(assets);
-    })
-  );
+self.addEventListener("install", (evt) => {
+   let cacheUrls = async () => {
+      const cache = await caches.open(cacheName);
+      return cache.addAll(assets);
+   };
+   evt.waitUntil(cacheUrls());
 });
+
 
 self.addEventListener('fetch', evt => {
   evt.respondWith(
